712c5900b5334aa08fd093639ff4feca27a18038
Anup Patel <apatel@ventanamicro.com>
2022-01-17 14:10:40 +0530

    RISC-V: Allow marking IPIs as suitable for remote FENCEs
    To do remote FENCEs (i.e. remote TLB flushes) using IPI calls on the
    RISC-V kernel, we need hardware mechanism to directly inject IPI from
    the supervisor mode (i.e. RISC-V kernel) instead of using SBI calls.
    
    The upcoming AIA IMSIC devices allow direct IPI injection from the
    supervisor mode (i.e. RISC-V kernel). To support this, we extend the
    riscv_ipi_set_virq_range() function so that IPI provider (i.e. irqchip
    drivers can mark IPIs as suitable for remote FENCEs.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    Reviewed-by: Atish Patra <atishp@rivosinc.com>

--- drivers/clocksource/timer-clint.c
+++ drivers/clocksource/timer-clint.c
@@ -249,7 +249,7 @@ static int __init clint_timer_init_dt(struct device_node *np)
 		goto fail_free_irq;
 	}
 
-	riscv_ipi_set_virq_range(virq, BITS_PER_BYTE);
+	riscv_ipi_set_virq_range(virq, BITS_PER_BYTE, true);
 	clint_clear_ipi(clint_ipi_irq, NULL);
 
 	return 0;
