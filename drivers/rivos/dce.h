4a369844fbbeb0ed5b4fdabd94ab9613af653b13
Dao Lu <daolu@rivosinc.com>
2022-11-01 14:28:02 -0700

    RIVOS: DCE: #if out no SVA/PASID operation
    Adaptation of Dao's:
    Prelminary commit, comment out the code only relevent for non-PASID operations

--- drivers/rivos/dce.h
+++ drivers/rivos/dce.h
@@ -5,6 +5,8 @@
 #include "linux/eventfd.h"
 #include <linux/workqueue.h>
 
+#define NOPASID 0
+
 #define DCE_CTRL 0
 
 #define DCE_STATUS 8
@@ -164,11 +166,13 @@ typedef struct DescriptorRing {
 	dma_addr_t desc_dma;
 	dma_addr_t hti_dma;
 
+#if NOPASID
 	/* Scatter/Gather list information
 	 * dynamically allocated table of queue size elms for each */
 	int * dma_direction[NUM_SG_TBLS];
 	struct sg_table * sg_tables[NUM_SG_TBLS];
 	DataAddrNode ** hw_addr[NUM_SG_TBLS];
+#endif
 } DescriptorRing;
 
 typedef struct UserArea {
