ecfa0d9beb661dbabaa62b88c01b49f0db086cee
Alexandre Ghiti <alexghiti@alexghiti.eu.rivosinc.com>
2023-01-30 10:44:15 +0000

    rivos: Introduce kernel CI
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

---
+++ rivos/linux_ci/tests/kasan/kasan_kunit_test/main.py
@@ -0,0 +1,15 @@
+def test_main(c):
+    c.sudo("modprobe kasan_test || true")
+    res = c.sudo("dmesg -c")
+
+    # KASAN_KUNIT_TEST
+    kasan_kunit_test_expect = "# kasan: pass:46 fail:1 skip:11 total:58"
+    if kasan_kunit_test_expect not in res.stdout:
+        result = ""
+        for line in res.stdout:
+            if re.match("# kasan: pass:", line):
+                result = line
+                break
+        raise RuntimeError("FAIL: KASAN_KUNIT_TEST results differ:\nexpected: {}\nresult: {}".format(kasan_kunit_test_expect, result))
+
+test_main(c)
