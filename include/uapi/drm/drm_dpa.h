84b4d81e180059e9dcb1cb93b1067b2319897fe0
Kristian H. Kristensen <krh@rivosinc.com>
2023-04-24 05:07:01 -0700

    dpa: Change uapi copyright to GPL-2.0 WITH Linux-syscall-note
    Signed-off-by: Kristian H. Kristensen <krh@rivosinc.com>

--- include/uapi/drm/drm_dpa.h
+++ include/uapi/drm/drm_dpa.h
@@ -1,20 +1,7 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
- * Rivos DPA device driver
- *
  * Copyright (C) 2022-2023 Rivos Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, see <http://www.gnu.org/licenses/>.
+ * Rivos DPA device driver
  */
 
 #ifndef __DRM_DPA_H__
