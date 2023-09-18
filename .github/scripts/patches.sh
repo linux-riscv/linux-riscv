30e34697f84fdc64953e6a2465248afdf71fb45f
Björn Töpel <bjorn@rivosinc.com>
2023-09-18 21:06:52 +0200

    adding ci files

---
+++ .github/scripts/patches.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Rivos Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+d=$(dirname "${BASH_SOURCE[0]}")
+
+. ${d}/helpers.sh
+
+basesha=$(git log -1 --pretty=%H .github/scripts/helpers.sh)
+patches=( $(git rev-list --reverse ${basesha}..HEAD) )
+
+rc=0
+cnt=1
+for i in "${patches[@]}"; do
+    tests=( $(ls ${d}/patches/*.sh) )
+    tcnt=1
+    for j in "${tests[@]}"; do
+        git reset --hard $i >/dev/null
+        msg="Patch ${cnt}/${#patches[@]}: Test ${tcnt}/${#tests[@]}: ${j}"
+        echo "::group::${msg}"
+        testrc=0
+        bash ${j} || testrc=1
+        echo "::endgroup::"
+        if (( $testrc )); then
+            rc=1
+            echo "::error::FAIL ${msg}"
+        else
+            echo "::notice::OK ${msg}"
+        fi
+        tcnt=$(( tcnt + 1 ))
+    done
+    cnt=$(( cnt + 1 ))
+done
+
+exit $rc
