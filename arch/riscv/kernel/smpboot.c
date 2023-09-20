7461faf04ad4520aa24468c92d2a10a7317c3359
Evan Green <evan@rivosinc.com>
2023-09-20 12:38:01 -0700

    RISC-V: Probe misaligned access speed in parallel
    Probing for misaligned access speed takes about 0.06 seconds. On a
    system with 64 cores, doing this in smp_callin() means it's done
    serially, extending boot time by 3.8 seconds. That's a lot of boot time.
    
    Instead of measuring each CPU serially, let's do the measurements on
    all CPUs in parallel. If we disable preemption on all CPUs, the
    jiffies stop ticking, so we can do this in stages of 1) everybody
    except core 0, then 2) core 0.
    
    The measurement call in smp_callin() stays around, but is now
    conditionalized to only run if a new CPU shows up after the round of
    in-parallel measurements has run. The goal is to have the measurement
    call not run during boot or suspend/resume, but only on a hotplug
    addition.
    
    Reported-by: Jisheng Zhang <jszhang@kernel.org>
    Closes: https://lore.kernel.org/all/mhng-9359993d-6872-4134-83ce-c97debe1cf9a@palmer-ri-x1c9/T/#mae9b8f40016f9df428829d33360144dc5026bcbf
    Fixes: 584ea6564bca ("RISC-V: Probe for unaligned access speed")
    Signed-off-by: Evan Green <evan@rivosinc.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Tested-by: Andrew Jones <ajones@ventanamicro.com>
    Tested-by: Jisheng Zhang <jszhang@kernel.org>
    Acked-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- arch/riscv/kernel/smpboot.c
+++ arch/riscv/kernel/smpboot.c
@@ -27,6 +27,7 @@
 #include <linux/sched/mm.h>
 #include <asm/cpu_ops.h>
 #include <asm/cpufeature.h>
+#include <asm/hwprobe.h>
 #include <asm/irq.h>
 #include <asm/mmu_context.h>
 #include <asm/numa.h>
@@ -246,7 +247,15 @@ asmlinkage __visible void smp_callin(void)
 
 	numa_add_cpu(curr_cpuid);
 	set_cpu_online(curr_cpuid, 1);
-	check_unaligned_access(curr_cpuid);
+
+	/*
+	 * Boot-time misaligned access speed measurements are done in parallel
+	 * in an initcall. Only measure here for hotplug.
+	 */
+	if ((system_state == SYSTEM_RUNNING) &&
+	    (per_cpu(misaligned_access_speed, curr_cpuid) == RISCV_HWPROBE_MISALIGNED_UNKNOWN)) {
+		check_unaligned_access(NULL);
+	}
 
 	if (has_vector()) {
 		if (riscv_v_setup_vsize())
