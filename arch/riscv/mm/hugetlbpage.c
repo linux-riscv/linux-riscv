7724086b43d8dbd63708fa3d3037b07cb7f8eba6
Alexandre Ghiti <alexghiti@rivosinc.com>
2023-09-28 17:18:46 +0200

    riscv: Fix set_huge_pte_at() for NAPOT mappings when a swap entry is set
    We used to determine the number of page table entries to set for a NAPOT
    hugepage by using the pte value which actually fails when the pte to set is
    a swap entry.
    
    So take advantage of a recent fix for arm64 reported in [1] which
    introduces the size of the mapping as an argument of set_huge_pte_at(): we
    can then use this size to compute the number of page table entries to set
    for a NAPOT region.
    
    Fixes: 82a1a1f3bfb6 ("riscv: mm: support Svnapot in hugetlb page")
    Reported-by: Ryan Roberts <ryan.roberts@arm.com>
    Closes: https://lore.kernel.org/linux-arm-kernel/20230922115804.2043771-1-ryan.roberts@arm.com/ [1]
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- arch/riscv/mm/hugetlbpage.c
+++ arch/riscv/mm/hugetlbpage.c
@@ -182,15 +182,22 @@ void set_huge_pte_at(struct mm_struct *mm,
 		     pte_t *ptep,
 		     pte_t pte)
 {
+	unsigned long hugepage_shift;
 	int i, pte_num;
 
-	if (!pte_napot(pte)) {
-		set_pte_at(mm, addr, ptep, pte);
-		return;
-	}
+	if (sz >= PGDIR_SIZE)
+		hugepage_shift = PGDIR_SHIFT;
+	else if (sz >= P4D_SIZE)
+		hugepage_shift = P4D_SHIFT;
+	else if (sz >= PUD_SIZE)
+		hugepage_shift = PUD_SHIFT;
+	else if (sz >= PMD_SIZE)
+		hugepage_shift = PMD_SHIFT;
+	else
+		hugepage_shift = PAGE_SHIFT;
 
-	pte_num = napot_pte_num(napot_cont_order(pte));
-	for (i = 0; i < pte_num; i++, ptep++, addr += PAGE_SIZE)
+	pte_num = sz >> hugepage_shift;
+	for (i = 0; i < pte_num; i++, ptep++, addr += (1 << hugepage_shift))
 		set_pte_at(mm, addr, ptep, pte);
 }
 
