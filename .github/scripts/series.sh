23dd4b0d61e0a3f8c3987f0c03d3e5c8cde1c205
Björn Töpel <bjorn@rivosinc.com>
2024-01-05 13:32:11 +0000

    adding ci files

---
+++ .github/scripts/series.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Rivos Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+d=$(dirname "${BASH_SOURCE[0]}")
+
+${d}/series/build_kernels.sh
+${d}/series/run_tests.sh
