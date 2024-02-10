f7a29d2a776b7b8c8d78855b64e20833fb6310ae
Zhang Bingwu <xtexchooser@duck.com>
2024-02-10 15:46:00 +0800

    kbuild: Abort make on install failures
    Setting '-e' flag tells shells to exit with error exit code immediately
    after any of commands fails, and causes make(1) to regard recipes as
    failed.
    
    Before this, make will still continue to succeed even after the
    installation failed, for example, for insufficient permission or
    directory does not exist.
    
    Signed-off-by: Zhang Bingwu <xtexchooser@duck.com>
    Reviewed-by: Nicolas Schier <nicolas@fjasle.eu>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- arch/riscv/boot/install.sh
+++ arch/riscv/boot/install.sh
@@ -17,6 +17,8 @@
 #   $3 - kernel map file
 #   $4 - default install path (blank if root directory)
 
+set -e
+
 if [ "$(basename $2)" = "Image.gz" ]; then
 # Compressed install
   echo "Installing compressed kernel"
