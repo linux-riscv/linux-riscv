84affd11eead2b2dc8667f1051d51d09cc40e306
Ben Dooks <ben.dooks@codethink.co.uk>
2024-09-17 14:08:52 +0100

    riscv: traps: make insn fetch common in unknown instruction
    Add the insn as the second argument to riscv_v_first_use_handler() form
    the trap handler so when we add more handlers we can do the fetch of the
    instruction just once.
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- arch/riscv/kernel/vector.c
+++ arch/riscv/kernel/vector.c
@@ -168,11 +168,8 @@ bool riscv_v_vstate_ctrl_user_allowed(void)
 }
 EXPORT_SYMBOL_GPL(riscv_v_vstate_ctrl_user_allowed);
 
-bool riscv_v_first_use_handler(struct pt_regs *regs)
+bool riscv_v_first_use_handler(struct pt_regs *regs, u32 insn)
 {
-	u32 __user *epc = (u32 __user *)regs->epc;
-	u32 insn = (u32)regs->badaddr;
-
 	if (!has_vector())
 		return false;
 
@@ -183,12 +180,6 @@ bool riscv_v_first_use_handler(struct pt_regs *regs)
 	/* If V has been enabled then it is not the first-use trap */
 	if (riscv_v_vstate_query(regs))
 		return false;
-
-	/* Get the instruction */
-	if (!insn) {
-		if (__get_user(insn, epc))
-			return false;
-	}
 
 	/* Filter out non-V instructions */
 	if (!insn_is_vector(insn))
