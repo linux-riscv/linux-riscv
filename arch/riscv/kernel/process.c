902e9cab4775dc3403528d7ef939c903f10fc8b6
Deepak Gupta <debug@rivosinc.com>
2024-10-01 09:06:21 -0700

    riscv/shstk: If needed allocate a new shadow stack on clone
    Userspace specifies CLONE_VM to share address space and spawn new thread.
    `clone` allow userspace to specify a new stack for new thread. However
    there is no way to specify new shadow stack base address without changing
    API. This patch allocates a new shadow stack whenever CLONE_VM is given.
    
    In case of CLONE_VFORK, parent is suspended until child finishes and thus
    can child use parent shadow stack. In case of !CLONE_VM, COW kicks in
    because entire address space is copied from parent to child.
    
    `clone3` is extensible and can provide mechanisms using which shadow stack
    as an input parameter can be provided. This is not settled yet and being
    extensively discussed on mailing list. Once that's settled, this commit
    will adapt to that.
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- arch/riscv/kernel/process.c
+++ arch/riscv/kernel/process.c
@@ -28,6 +28,7 @@
 #include <asm/vector.h>
 #include <asm/cpufeature.h>
 #include <asm/exec.h>
+#include <asm/usercfi.h>
 
 #if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
 #include <linux/stackprotector.h>
@@ -203,7 +204,7 @@ int arch_dup_task_struct(struct task_struct *dst, struct task_struct *src)
 
 void exit_thread(struct task_struct *tsk)
 {
-
+	shstk_release(tsk);
 }
 
 int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
@@ -211,6 +212,7 @@ int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
 	unsigned long clone_flags = args->flags;
 	unsigned long usp = args->stack;
 	unsigned long tls = args->tls;
+	unsigned long ssp = 0;
 	struct pt_regs *childregs = task_pt_regs(p);
 
 	memset(&p->thread.s, 0, sizeof(p->thread.s));
@@ -225,11 +227,18 @@ int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
 		p->thread.s[0] = (unsigned long)args->fn;
 		p->thread.s[1] = (unsigned long)args->fn_arg;
 	} else {
+		/* allocate new shadow stack if needed. In case of CLONE_VM we have to */
+		ssp = shstk_alloc_thread_stack(p, args);
+		if (IS_ERR_VALUE(ssp))
+			return PTR_ERR((void *)ssp);
+
 		*childregs = *(current_pt_regs());
 		/* Turn off status.VS */
 		riscv_v_vstate_off(childregs);
 		if (usp) /* User fork */
 			childregs->sp = usp;
+		/* if needed, set new ssp */
+		ssp ? set_active_shstk(p, ssp) : 0;
 		if (clone_flags & CLONE_SETTLS)
 			childregs->tp = tls;
 		childregs->a0 = 0; /* Return value of fork() */
