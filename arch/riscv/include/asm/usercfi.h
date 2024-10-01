f69e95248cd87fe5d4cf9aaf93de5e84a3086a53
Deepak Gupta <debug@rivosinc.com>
2024-10-01 09:06:25 -0700

    riscv: Implements arch agnostic indirect branch tracking prctls
    prctls implemented are:
    PR_SET_INDIR_BR_LP_STATUS, PR_GET_INDIR_BR_LP_STATUS and
    PR_LOCK_INDIR_BR_LP_STATUS.
    
    On trap entry, ELP state is recorded in sstatus image on stack and SR_ELP
    in CSR_STATUS is cleared.
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- arch/riscv/include/asm/usercfi.h
+++ arch/riscv/include/asm/usercfi.h
@@ -16,7 +16,9 @@ struct kernel_clone_args;
 struct cfi_status {
 	unsigned long ubcfi_en : 1; /* Enable for backward cfi. */
 	unsigned long ubcfi_locked : 1;
-	unsigned long rsvd : ((sizeof(unsigned long)*8) - 2);
+	unsigned long ufcfi_en : 1; /* Enable for forward cfi. Note that ELP goes in sstatus */
+	unsigned long ufcfi_locked : 1;
+	unsigned long rsvd : ((sizeof(unsigned long)*8) - 4);
 	unsigned long user_shdw_stk; /* Current user shadow stack pointer */
 	unsigned long shdw_stk_base; /* Base address of shadow stack */
 	unsigned long shdw_stk_size; /* size of shadow stack */
@@ -33,6 +35,10 @@ bool is_shstk_locked(struct task_struct *task);
 bool is_shstk_allocated(struct task_struct *task);
 void set_shstk_lock(struct task_struct *task);
 void set_shstk_status(struct task_struct *task, bool enable);
+bool is_indir_lp_enabled(struct task_struct *task);
+bool is_indir_lp_locked(struct task_struct *task);
+void set_indir_lp_status(struct task_struct *task, bool enable);
+void set_indir_lp_lock(struct task_struct *task);
 
 #define PR_SHADOW_STACK_SUPPORTED_STATUS_MASK (PR_SHADOW_STACK_ENABLE)
 
@@ -57,6 +63,14 @@ void set_shstk_status(struct task_struct *task, bool enable);
 #define set_shstk_lock(task)
 
 #define set_shstk_status(task, enable)
+
+#define is_indir_lp_enabled(task) false
+
+#define is_indir_lp_locked(task) false
+
+#define set_indir_lp_status(task, enable)
+
+#define set_indir_lp_lock(task)
 
 #endif /* CONFIG_RISCV_USER_CFI */
 
