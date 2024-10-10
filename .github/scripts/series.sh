0571e692273c7118aaa79660a0f7731ef974a268
Björn Töpel <bjorn@rivosinc.com>
2024-10-10 15:29:48 +0000

    adding ci files

---
+++ .github/scripts/series.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Rivos Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+d=$(dirname "${BASH_SOURCE[0]}")
+. $d/series/utils.sh
+
+
+logs=$(get_logs_dir)
+f=${logs}/series.log
+
+date -Iseconds | tee -a ${f}
+echo "Build, and boot various kernels" | tee -a ${f}
+echo "Top 16 commits" | tee -a ${f}
+git log -16 --abbrev=12 --pretty="commit %h (\"%s\")" | tee -a ${f}
+
+${d}/series/build_all.sh | tee -a ${f}
+${d}/series/test_all.sh | tee -a ${f}
